6ec633f46397b4c00a7f0cabfbf2915e3029bea0
CheckToSee <138322844+CheckToSee@users.noreply.github.com>
2023-10-07 13:07:08 -0400

    Update Remove Duplicates from Sorted Array.py

--- Remove Duplicates from Sorted Array.py
+++ Remove Duplicates from Sorted Array.py
@@ -1,20 +1,16 @@
-# FIXME: make it work run properly in leetcode
 class Solution:
     def removeDuplicates(nums):
-        numLength = len(nums)
+        numsLength = len(nums)
         trail = 0
         k = 0
         dupCheck = []
-        for i in range(numLength):
+
+        for i in range(numsLength):
             if nums[i] not in dupCheck:
                 dupCheck.append(nums[i])
+                nums[k] = dupCheck[k]
                 k += 1
             else:
                 trail += 1
-        for i in range(trail):
-            dupCheck.append('_')
-        nums = dupCheck
-        print(nums)
-        return k
 
-print(Solution.removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+        return k
