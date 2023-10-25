36f3d21e4f470b524ede35cb3bbf586dc849ff2e
CheckToSee <138322844+CheckToSee@users.noreply.github.com>
2023-10-25 17:40:43 -0400

    added Reverse Vowels of a String

---
+++ Reverse Vowels of a String.py
@@ -0,0 +1,35 @@
+class Solution:
+    def reverseVowels(s: str) -> str:
+        length = len(s)
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        v_in_word = []
+        pos = []
+
+        for i in range(length):
+            if s[i] in vowels:
+                pos.append(i)
+                v_in_word.append(s[i])
+        if pos:
+            pos_length = len(pos)
+            temp = []
+            mut_s = list(s)
+            for j in range(pos_length):
+                rev = pos_length-j-1
+                temp.append(pos[rev])
+                print(rev)
+
+            for k in range(pos_length):
+                mut_s[temp[k]] = v_in_word[k]
+            s = "".join(mut_s)
+            print(s)
+            return s
+        else:
+            return ""
+
+        # print(v_in_word)
+        # print(pos)
+        # print(temp)
+        # print(s)
+
+    reverseVowels("123456")
+    reverseVowels("hello")
